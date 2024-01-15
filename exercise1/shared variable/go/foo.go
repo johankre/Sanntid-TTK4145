6c3d11af6e4b81d37a8a013f815369fdaa60a113
Linus <125355331+Linusludescher@users.noreply.github.com>
2024-01-15 14:36:14 +0100

    fuck update

--- exercise1/shared variable/go/foo.go
+++ exercise1/shared variable/go/foo.go
@@ -5,47 +5,48 @@ package main
 import (
 	. "fmt"
 	"runtime"
-	"sync"
-	"time"
+	//"time"
 )
 
 var i = 0
 
-// Mutexes for synchronization
-var mutex sync.Mutex
-
-func incrementing() {
-	//TODO: increment i 1000000 times
+func incrementing(incr chan int) {
 	for j := 0; j <= 1000000; j++ {
-		mutex.Lock()
-		i++
-		mutex.Unlock()
+		i += 1
+		incr <- i
 	}
 }
 
-func decrementing() {
-	//TODO: decrement i 1000000 times
-	for j := 1000000; j >= 0; j-- {
-		mutex.Lock()
-		i--
-		mutex.Unlock()
+func decrementing(decr chan int) {
+	for j := 0; j <= 1000001; j++ {
+		i -= 1
+		decr <- i
 	}
 
 }
 
 func main() {
 	// What does GOMAXPROCS do? What happens if you set it to 1?
-	// 1 gives a sequential program.
-	// 2 or more gives a concurrently executing program.
-	// Answer in results.md
 	runtime.GOMAXPROCS(2)
 
 	// TODO: Spawn both functions as goroutines
-	go incrementing()
-	go decrementing()
+	incr := make(chan int)
+	decr := make(chan int)
+
+	go incrementing(incr)
+	go decrementing(decr)
+
+	for {
+		select {
+		case <-incr:
+			i := <-incr
+		case <-decr:
+			i := <-decr
+		}
+	}
 
 	// We have no direct way to wait for the completion of a goroutine (without additional synchronization of some sort)
 	// We will do it properly with channels soon. For now: Sleep.
-	time.Sleep(500 * time.Millisecond)
+	// time.Sleep(500 * time.Millisecond)
 	Println("The magic number is:", i)
 }
