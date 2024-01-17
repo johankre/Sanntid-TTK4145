f23eeee14857285505618c36f9aab97028626b06
johankre <johannes.k.reitan@gmail.com>
2024-01-17 17:08:20 +0100

    test udpListener

--- Exercise 2/udpListener.go
+++ Exercise 2/udpListener.go
@@ -3,7 +3,12 @@ package main
 import (
 	"fmt"
 	"net"
+	"strconv"
+	"time"
 )
+
+// Globals
+const UDPSendPort string = ":20024"
 
 func listener(port string) {
 	pc, err := net.ListenPacket("udp", port)
@@ -19,15 +24,32 @@ func listener(port string) {
 		if err != nil {
 			panic(err)
 		}
-        fmt.Println(addr.String())
+		// print IP addres
+		fmt.Println(addr.String())
 		if n != 0 {
+			// print msg. on port
 			fmt.Println(string(buf[:n]))
-            break
+			break
 		}
 	}
 
 }
+func udpSend() {
+	conn, err := net.Dial("udp", UDPSendPort)
+	if err != nil {
+		panic(err)
+	}
+	n := 0
+	for {
+		t := strconv.Itoa(n)
+		msg := "Hellow world!, " + t
+		conn.Write([]byte(msg))
+		n++
+		time.Sleep(100)
+	}
+}
 
 func main() {
-	listener(":30000")
+	// listener(":30000")
+	udpSend()
 }
