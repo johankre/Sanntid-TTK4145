242663822c4aaaf6b06d9a17c789daa5530b9759
johankre <johannes.k.reitan@gmail.com>
2024-01-17 15:58:06 +0100

    udpListener

---
+++ Exercise 2/udpListener.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func listener(){
+    l, err := net.Listen("udp", ":30000")
+    if err != nil {
+        panic(err)
+    }
+    defer l.Close()
+    
+    for {
+        // Listen for connection
+        reader := bufio.NewReader(os.Stdin)
+        data, err := reader.ReadString('\n')
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println(data)
+}
+}
