ab51d7e960a7e046b6a13ae2ea359fcf64783dee
haavaks <haavard.akselsen@gmail.com>
2024-01-17 18:02:58 +0100

    halla

---
+++ Exercise 2/tcp.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func sendTCPMessage(message string, serverAddr string) {
+	conn, err := net.Dial("tcp", serverAddr)
+	if err != nil {
+		fmt.Println("Error connecting:", err)
+		return
+	}
+	defer conn.Close()
+
+	// Convert the message to bytes
+	messageBytes := []byte(message)
+
+	// Send the message to the server
+	_, err = conn.Write(messageBytes)
+	if err != nil {
+		fmt.Println("Error writing:", err)
+		return
+	}
+
+	// Read the response from the server
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return
+	}
+
+	fmt.Printf("Server response: %s\n", buffer[:n])
+}
+
+func main() {
+	serverAddr := "127.0.0.1:12345" // Set the server address and port
+
+	// Send a message to the server
+	message := "Hello, TCP Server!"
+	sendTCPMessage(message, serverAddr)
+}
