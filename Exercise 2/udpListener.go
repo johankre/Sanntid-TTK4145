fa64c48ded810edc2c9c1509397c2def097aeeab
johankre <johannes.k.reitan@gmail.com>
2024-01-17 15:59:19 +0100

    fmt udpListener

--- Exercise 2/udpListener.go
+++ Exercise 2/udpListener.go
@@ -7,20 +7,20 @@ import (
 	"os"
 )
 
-func listener(){
-    l, err := net.Listen("udp", ":30000")
-    if err != nil {
-        panic(err)
-    }
-    defer l.Close()
-    
-    for {
-        // Listen for connection
-        reader := bufio.NewReader(os.Stdin)
-        data, err := reader.ReadString('\n')
-    if err != nil {
-        panic(err)
-    }
-    fmt.Println(data)
-}
+func listener() {
+	l, err := net.Listen("udp", ":30000")
+	if err != nil {
+		panic(err)
+	}
+	defer l.Close()
+
+	for {
+		// Listen for connection
+		reader := bufio.NewReader(os.Stdin)
+		data, err := reader.ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(data)
+	}
 }
